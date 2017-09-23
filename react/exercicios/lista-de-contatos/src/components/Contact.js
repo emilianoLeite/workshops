abd63de70bb600935849cf75ba1c5573618a72ad
Emiliano Leite <emiliano.leite@codus.com.br>
2017-09-23 12:11:27 -0300

    Remove unused 'Component' import.

--- react/exercicios/lista-de-contatos/src/components/Contact.js
+++ react/exercicios/lista-de-contatos/src/components/Contact.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 function Contact(props) {
   return <span>{props.name}; {props.telephone} </span> ;
--- react/exercicios/lista-de-contatos/src/components/List.js
+++ react/exercicios/lista-de-contatos/src/components/List.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Contact from './Contact.js';
 
 class List extends React.Component {
